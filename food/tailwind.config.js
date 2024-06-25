b01c05586afe02bf63021788cb1a742b36e823fb
Mond <126099833+e-mond@users.noreply.github.com>
2024-06-25 23:36:10 +0000

    Day 17

---
+++ food/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust this as necessary
+    './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
